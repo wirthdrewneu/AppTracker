fe0984c58fd054a4851245f1ba6fe0e158934ff6
Harman Sidhu <harmandeepsinghful@gmail.com>
2020-11-03 06:22:41 -0800

    Nodemon error potential fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const indexRouter = require("./routes/index");
+// const indexRouter = require("./routes/index");
 
 const app = express();
 
@@ -12,9 +12,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "interapp/build")));
 
-app.use("/", indexRouter);
-app.get("/*", function(req,res){
-	res.sendFile(path.join(__dirname,"interapp/build","index.html"));
-});
+// app.use("/", indexRouter);
+// app.get("/*", function(req,res){
+// 	res.sendFile(path.join(__dirname,"interapp/build","index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, "./reactapp/build")));
+app.get("*", (request, response) => {
+	response.sendFile(path.join(__dirname, "./reactapp/build", "index.html"));
+}); 
 
 module.exports = app;
