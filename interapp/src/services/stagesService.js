62dc667b958f593622f8d5f6452d2a146874e3f7
Harman Sidhu <harmandeepsinghful@gmail.com>
2020-11-03 11:44:10 -0800

    Fixed follow up filter bug

--- interapp/src/services/stagesService.js
+++ interapp/src/services/stagesService.js
@@ -2,7 +2,7 @@ export const stages = [
     {id: 1, stage: "Applied"},
     {id: 2, stage: "Online Assessment"},
     {id: 3, stage: "Interview"},
-    {id: 4, stage: "Follow-up"},
+    {id: 4, stage: "Follow Up"},
     {id: 5, stage: "Offer"},
     {id: 6, stage: "Accepted"},
     {id: 7, stage: "Rejected"},
